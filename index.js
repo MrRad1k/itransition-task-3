dd4dbecd265f78d1b7bfc2c51947a1253e5bcede
MrRad1k <68542732+MrRad1k@users.noreply.github.com>
2022-02-18 23:27:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path');
+require('dotenv').config();
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('public'))
-
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routers/authRouter'))
