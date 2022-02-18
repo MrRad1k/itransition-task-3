0117ee1f68f8e6391913eb948efdff16d5bca557
MrRad1k <68542732+MrRad1k@users.noreply.github.com>
2022-02-18 23:51:34 +0300

    task3

---
+++ routers/authRouter.js
@@ -0,0 +1,84 @@
+const { Router } = require('express')
+const router = Router()
+const User = require('../models/User')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+router.get('/',
+    async (req, res) => {
+        const user = await User.find()
+        res.status(200).send(user)
+    }
+)
+
+router.post('/reg',
+    [
+        check('email', 'Incorrect email').isEmail()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Incorrect registration data!'
+                })
+            }
+
+            const { name, email, password, date, status } = req.body
+
+            const isUsed = await User.findOne({ email })
+
+            if (isUsed) {
+                return res.status(300).json({ message: 'Email busy.' })
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12)
+            const dateReg = new Date().toLocaleDateString()
+
+            const user = new User({
+                name, email, password: hashedPassword, date: dateReg, status: false
+            })
+
+            await user.save()
+
+            res.status(201).json({ message: 'User created.' })
+
+        } catch (err) {
+            console.log(err)
+        }
+    })
+
+    router.post('/login', async (req, res) => {
+        const user = await User.findOne({
+            email: req.body.email,
+            userStatus: req.body.status
+        })
+    
+        if (!user) {
+            return { status: 'error', error: 'Invalid login', userStatus: user.status }
+        }
+    
+        const isPasswordValid = await bcrypt.compare(
+            req.body.password,
+            user.password
+        )
+    
+        if (isPasswordValid) {
+            const token = jwt.sign(
+                {
+                    userId: user.id,
+                    email: user.email,
+                    userStatus: user.status
+                },
+                'secret123'
+            )
+    
+            return res.json({ status: 'ok', token, userStatus: user.status, email: user.email })
+        } else {
+            return res.json({ status: 'error', user: false })
+        }
+    })
+
+module.exports = router
